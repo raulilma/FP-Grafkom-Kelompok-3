75fe46f871cea19d80d330fa6826fc88ada1851f
Raul Ilma Rajasa <raulilmarajasa@gmail.com>
2022-12-16 14:49:55 +0700

    feat: registration for players with firebase and its data structure

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -13,3 +13,118 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const database = firebase.database();
+
+function logout() {
+  firebase
+      .auth()
+      .signOut()
+      .then(function () {
+          alert("Logout Berhasil!");
+          window.location.href = "login";
+      })
+      .catch(function (error) {
+          alert("Logout Error! " + error);
+      });
+}
+
+function register() {
+  email = document.getElementById("email_reg").value;
+  password = document.getElementById("password_reg").value;
+  full_name = document.getElementById("namalengkap").value;
+  username = document.getElementById("username").value;
+  if (
+      validate_email(email) == false ||
+      validate_password(password) == false
+  ) {
+      alert("Periksa lagi password maupun emailmu!");
+      return;
+  }
+  if (validate_field(full_name) == false || validate_field(username) == false) {
+      alert("Periksa lagi nama lengkap dan juga usernamemu!");
+      return;
+  }
+  auth.createUserWithEmailAndPassword(email, password)
+  .then(function () {
+      auth.createUserWithEmailAndPassword(email, password);
+      var user = auth.currentUser;
+      var database_ref = database.ref();
+      var user_data = {
+          Username: username,
+          BestScore: 0,
+          Email: email,
+          NamaLengkap: full_name,
+          LastLogin: Date.now(),
+      };
+      database_ref.child("Players/" + user.uid).set(user_data);
+      user.sendEmailVerification()
+      .then(function () {
+          alert(
+              "Link verifikasi sudah terkirim di emailmu. Klik link tersebut untuk dapat mengakses Rendora!"
+          );
+      })
+      .catch(function (error) {
+          alert("Error: " + error + "!");
+      });
+  })
+  .catch(function (error) {
+      var error_message = error.message;
+      alert(error_message);
+  });
+}
+
+function login() {
+  email = document.getElementById("email").value;
+  password = document.getElementById("password").value;
+  if (
+      validate_email(email) == false ||
+      validate_password(password) == false
+  ) {
+      alert("Email or Password is Outta Line!!");
+      return;
+  }
+
+  auth.signInWithEmailAndPassword(email, password)
+      .then(function () {
+          var user = auth.currentUser;
+          var database_ref = database.ref();
+          var user_data = {
+              LastLogin: Date.now(),
+          };
+          database_ref.child("Players/" + user.uid).update(user_data);
+          window.location.href = "onboarding.html";
+      })
+      .catch(function (error) {
+          var error_message = error.message;
+          alert(error_message);
+      });
+}
+
+// validasi input user di webnya
+function validate_email(email) {
+  expression = /^[^@]+@\w+(\.\w+)+\w$/;
+  if (expression.test(email) == true) {
+      return true;
+  } else {
+      return false;
+  }
+}
+
+function validate_password(password) {
+  if (password < 6) {
+      return false;
+  } else {
+      return true;
+  }
+}
+
+function validate_field(field) {
+  if (field == null) {
+      return false;
+  }
+
+  if (field.length <= 0) {
+      return false;
+  } else {
+      return true;
+  }
+}
